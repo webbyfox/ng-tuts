0c431c3cda5af684207feff482f30f8edf729e32
=Rizwan Mansuri <=rmansuri@rcseng.ac.uk>
2016-12-08 11:26:04 +0000

    Added interval service

--- Github API viewer/script.js
+++ Github API viewer/script.js
@@ -4,7 +4,7 @@
 
 var app = angular.module("gitHubViewer", []);	
 
-var MainController = function($scope, $http){
+var MainController = function($scope, $http, $interval, $log){
 
 	var onUserComplete = function(response){
 		$scope.user = response.data;
@@ -23,14 +23,32 @@ var MainController = function($scope, $http){
 	$scope.username="angular";   
 	
 	$scope.search = function(){
+		$log.info("searching for ..." + $scope.username);
  		$http.get('https://api.github.com/users/' + $scope.username)
     	.then(onUserComplete, onError);
+    	if(countdownInterval){
+    		$interval.cancel(countdownInterval);
+    		$scope.countdown = null;
+    	}
 	};
 
-	
+	var descrementCountDown  = function(){
+		$scope.countdown -=1;
+		if($scope.countdown < 1){
+			$scope.search($scope.username);
+		}
+	};
+
+	$scope.setInterval
+	var countdownInterval = null;
+	var startCountdown = function(){
+		countdownInterval = $interval(descrementCountDown, 1000,$scope.countdown);
+	};
  	
 	$scope.message = "Github Viewer";
   	$scope.repoSortOrder = "-stargazers_count";
+  	$scope.countdown = 5;
+  	startCountdown();
 };
 app.controller("MainController", MainController);
 }());
